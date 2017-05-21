533652537d2ce02aa6a2547757aa27240918be49
u4bi <u4bi@u4bi.com>
2017-05-21 10:49:36 +0900

    uglify 패키지 모듈 추가
    - rollup-plugin-uglify
    - uglify-es (uglify 작업중 es6 호환 작업을 위한 추가 패키지)

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,13 @@
+import uglify from 'rollup-plugin-uglify'; /* https://github.com/TrySound/rollup-plugin-uglify */
+import { minify } from 'uglify-es';
+
 export default { 
     entry: './src/core/App.js', 
     dest: './dist/js/bundle.js', 
     format: 'umd',      /* https://github.com/rollup/rollup/wiki/JavaScript-API#format */
     moduleName: 'U4bi', /* https://github.com/rollup/rollup/wiki/JavaScript-API#modulename */
     sourceMap: 'inline',
-    plugins: []
+    plugins: [
+        uglify({}, minify)
+    ]
 };
